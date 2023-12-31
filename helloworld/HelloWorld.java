7ca8187df11e048e3b849befa56cc1336318fdfa
RenanMRb <renan.costa.bsi@gmail.com>
2023-12-31 13:18:10 -0300

    primeiro treino - 'print 'Hello World'

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush(); 
+        System.out.println("Hello World!");
+    }
+}
